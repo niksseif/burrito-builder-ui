4e8df2e88205921dd9865096b3d7a5d0575084f1
NikSS <nik.safe@gmail.com>
2020-12-11 17:17:48 -0800

    added App.test.js, add npm packeges for testing,removed yarn.lock, removed package-lock and node modules for some node packages dependency issue

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,15 +1,17 @@
+import OrderForm from '../OrderForm/OrderForm.js'
 import React from 'react';
+import App from './App'
 import { shallow } from 'enzyme'
+import { expect } from 'chai';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
 
-import OrderForm from '../OrderForm/OrderForm.js'
-import App from './App'
 
-describe('<App/>', () =>{
-  it('renders without crashing', () => {
+//shallow testing
+describe('Render <App/> ', () =>{
+    it('renders without crashing', () => {
     const wrapper = shallow((<App><OrderForm/></App>))
-    expect(wrapper.contains(<OrderForm/>)).to.equal(true)
+    expect(wrapper.contains(<OrderForm/>)).to.equal(true);
   })
 })
