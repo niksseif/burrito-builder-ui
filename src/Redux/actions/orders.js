665f647b81d121689cccac39476720e9d560ed7b
NikSS <nik.safe@gmail.com>
2020-12-11 16:12:48 -0800

    moved possible ingrediants to component state, add handleAddName and handleIngrediants change

--- src/Redux/actions/orders.js
+++ src/Redux/actions/orders.js
@@ -3,6 +3,7 @@ import { getOrders } from '../../apiCalls'
 export const GET_ORDERS_PENDING = "GET_ORDERS_PENDING";
 export const GET_ORDERS_SUCCESS = "GET_ORDERS_SUCCESS";
 export const GET_ORDERS_FAILED = "GET_ORDERS_FAILED";
+
 // Get the orders and set the dispatch payload based on the result.
 export const setOrders = () => {
   return async dispatch => {
--- src/components/OrderForm/OrderForm.js
+++ src/components/OrderForm/OrderForm.js
@@ -1,55 +1,92 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
+
 
 class OrderForm extends Component {
-  constructor(props) {
-    super();
-    this.props = props;
-    this.state = {
-      name: '',
-      ingredients: []
-    };
-  }
-
-
-  handleSubmit = e => {
+  state = {
+    ingrediants: [],
+    name: "",
+    possibleIngredients: [
+      "beans",
+      "steak",
+      "carnitas",
+      "sofritas",
+      "lettuce",
+      "queso fresco",
+      "pico de gallo",
+      "hot sauce",
+      "guacamole",
+      "jalapenos",
+      "cilantro",
+      "sour cream",
+    ],
+  };
+  // adding the ingrediants to the state 
+  handleIngredientChange = (ingrediant, e) => {
     e.preventDefault();
-    this.clearInputs();
-  }
-
-  clearInputs = () => {
-    this.setState({name: '', ingredients: []});
-  }
-
-  render() {
-    const possibleIngredients = ['beans', 'steak', 'carnitas', 'sofritas', 'lettuce', 'queso fresco', 'pico de gallo', 'hot sauce', 'guacamole', 'jalapenos', 'cilantro', 'sour cream'];
-    const ingredientButtons = possibleIngredients.map(ingredient => {
-      return (
-        <button key={ingredient} name={ingredient} onClick={e => this.handleIngredientChange(e)}>
-          {ingredient}
-        </button>
-      )
+    this.setState({
+      ingrediants: [...this.state.ingrediants, ingrediant], 
     });
-
+  
+  };
+// handle name change in the form 
+  handleAddName = (e) => {
+    e.preventDefault();
+    this.setState({ name: e.target.value });
+  };
+ 
+  render() {
     return (
-      <form>
-        <input
-          type='text'
-          placeholder='Name'
-          name='name'
+      <form className="container-form">
+        <h3>Order Menu</h3>
+        <input 
+          className ="name-input"
+          type="text"
+          placeholder="Name"
+          name="name"
           value={this.state.name}
-          onChange={e => this.handleNameChange(e)}
+          onChange={(e) => this.handleAddName(e)}
         />
 
-        { ingredientButtons }
+        {this.state.possibleIngredients.map((ingrediant, idx) => {
+          return (
+            <React.Fragment key={idx}>
+              <button
+                className="ingrediant"
+                onClick={(e) => this.handleIngredientChange(ingrediant, e)}
+                data-testid={`ingrediant${idx}`}
+              >
+                {ingrediant} ${this.state.cost}
+              </button>
+            </React.Fragment>
+        )}
+        )
+        }
 
-        <p>Order: { this.state.ingredients.join(', ') || 'Nothing selected' }</p>
-
-        <button onClick={e => this.handleSubmit(e)}>
+        <button
+        className='submit'
+          onClick={(e) => this.handleSubmit(e)}
+          disabled={
+            this.state.name === "" ||
+            this.state.ingrediants.length < 1 ||
+            this.props.data.isLoading
+              ? true
+              : false
+          }
+          data-testid="submit"
+        >
           Submit Order
         </button>
+        {// rendering added ingrediants for users to be able to see their added ingrediants
+          this.state.ingrediants.map(ing =>(<><ul>{ing}</ul></>))
+        }
       </form>
-    )
+    );
   }
 }
+
+
+
+
 
 export default OrderForm;
